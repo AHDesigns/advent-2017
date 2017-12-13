ff8a0bac3123b3b813fc6b43d29bb00b2a3d2f5c
Adam <adam.hall@sky.uk>
2017-12-13 08:22:04 +0000

    attempting functional ramda

--- 4_high_entropy_passphrases.js
+++ 4_high_entropy_passphrases.js
@@ -1,4 +1,11 @@
 var fs = require('fs');
+var R = require('ramda');
+
+function readFile(file) { return new Promise((resolve, reject) => {
+        fs.readFile(file, 'utf8', function(err,data){
+                err ? reject(err) : resolve(data);
+        })
+})};
 
 function duplicateWords(arr) {
         return arr.some((arrElement, i) => (
@@ -10,15 +17,25 @@ function anagramInArr(arr) {
         return duplicateWords(arr.map(word => word.split('').sort().join('')));
 }
 
-function doStuff(data) {
-        return data.split('\n')
-                .map(pwd => pwd.split(' '))
-                .filter(pass => (!duplicateWords(pass) && !anagramInArr(pass)));
+function stringTo2dArray(string) {
+        return string.split('\n').map(phrase => phrase.split(' '))
+}
+
+function findAnswer(data, method) {
+        return data.filter(pass => (method(pass)));
 };
 
-fs.readFile('./4_data.txt', 'utf8', function(err,data){
-        if (err) return;
-        else {
-                console.log(doStuff(data).length);
-        }
-})
+var answer = (arr) => arr.length;
+var noDuplicates = (data) => data.filter(arr => !duplicateWords(arr));
+var noDuplicatesOrAnagrams = (data) => data.filter(arr => !duplicateWords(arr) && !anagramInArr(arr))
+
+const evaluate = (data, method) => R.composeP(
+        answer,
+        method,
+        stringTo2dArray,
+        readFile
+)(data).then(a => console.log(a)).catch(err => console.log(err));
+
+evaluate('./4_data.txt', noDuplicates)
+
+evaluate('./4_data.txt', noDuplicatesOrAnagrams)
