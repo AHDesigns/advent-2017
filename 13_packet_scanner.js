04a56e24a4715c3c07088039c4d65b453dda0808
Adam <adam.hall@sky.uk>
2017-12-13 21:16:15 +0000

    started 13

---
+++ 13_packet_scanner.js
@@ -0,0 +1,18 @@
+const R = require('ramda');
+
+const { readFile, printP } = require('./utils');
+
+const stringToObject = (string) => {
+        const obj = {};
+        string.split('\n').map(line => {
+                
+        })
+} 
+// const stringTo2dArray = (string) => string.split('\n').map(phrase => phrase.split(' '));
+
+const evaluate = (data) => R.composeP(
+        stringToObject,
+        readFile
+)(data);
+
+printP(evaluate('./13_data.txt'));
