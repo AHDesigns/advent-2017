d4265e14cf9758e9a9fb272a8d0e9138e6b14cb8
Adam Hall <adamhalldesigns@gmail.com>
2018-04-09 19:26:26 +0100

    tidied number 1

--- 1_captcha.js
+++ 1_captcha.js
@@ -1,21 +1,23 @@
 const { check } = require('./utils');
 
-const inputs = [
+const part1 = [
         ["1122", 3],
         ["91212129", 9],
         ["1111", 4],
         ["1234", 0],
 ];
 
-// var input1 = ["1212", 6];
-// var input2 = ["1221", 0];
-// var input3 = ["123425", 4];
-// var input4 = ["123123", 12];
+const part2 = [
+        ["1212", 6],
+        ["1221", 0],
+        ["123425", 4],
+        ["123123", 12]
+];
 
 function captcha(numberString, ishalf) {
-        var arrOfNums = stringToArray(numberString);
-        var doubleCheck = arrOfNums.concat(arrOfNums);
-        var halfLifeMinusOne = ishalf ? (arrOfNums.length / 2) - 1 : 0;
+        const arrOfNums = stringToArray(numberString);
+        const doubleCheck = arrOfNums.concat(arrOfNums);
+        const halfLifeMinusOne = ishalf ? (arrOfNums.length / 2) - 1 : 0;
 
         return [0].concat(arrOfNums).reduce((acc, curr, i) => {
                 return curr === doubleCheck[i+halfLifeMinusOne] ? acc + curr : acc;
@@ -23,9 +25,10 @@ function captcha(numberString, ishalf) {
 }
 
 function stringToArray(numberString) {
-        var numsAsArray = numberString.split('');
-        var numsAsNums = numsAsArray.map(n => parseInt(n));
+        const numsAsArray = numberString.split('');
+        const numsAsNums = numsAsArray.map(n => parseInt(n));
         return numsAsNums;
 }
 
-inputs.forEach(input => check(input, captcha));
+part1.forEach(input => check(input, captcha));
+part2.forEach(input => check(input, captcha));
