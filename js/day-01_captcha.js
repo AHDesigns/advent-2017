0a3aeb30f8dd9c7e6c41b39cd81118e99e13feae
Adam Hall <adamhalldesigns@gmail.com>
2018-04-22 21:02:45 +0100

    tidyup

--- js/day-01_captcha.js
+++ js/day-01_captcha.js
@@ -1,34 +1,34 @@
-const { check } = require('./utils');
+const { check, readFile, stringToArray } = require('./utils');
 
 const part1 = [
-        ["1122", 3],
-        ["91212129", 9],
-        ["1111", 4],
-        ["1234", 0],
+    ['1122', 3],
+    ['91212129', 9],
+    ['1111', 4],
+    ['1234', 0],
 ];
 
 const part2 = [
-        ["1212", 6],
-        ["1221", 0],
-        ["123425", 4],
-        ["123123", 12]
+    ['1212', 6],
+    ['1221', 0],
+    ['123425', 4],
+    ['123123', 12],
 ];
 
+
 function captcha(numberString, ishalf) {
-        const arrOfNums = stringToArray(numberString);
-        const doubleCheck = arrOfNums.concat(arrOfNums);
-        const halfLifeMinusOne = ishalf ? (arrOfNums.length / 2) - 1 : 0;
+    const arrOfNums = stringToArray(numberString, '');
+    const doubleCheck = arrOfNums.concat(arrOfNums);
+    const halfLifeMinusOne = ishalf ? (arrOfNums.length / 2) - 1 : 0;
 
-        return [0].concat(arrOfNums).reduce((acc, curr, i) => {
-                return curr === doubleCheck[i+halfLifeMinusOne] ? acc + curr : acc;
-        })
-}
-
-function stringToArray(numberString) {
-        const numsAsArray = numberString.split('');
-        const numsAsNums = numsAsArray.map(n => parseInt(n));
-        return numsAsNums;
+    return [0].concat(arrOfNums).reduce((acc, curr, i) => (
+        curr === doubleCheck[i + halfLifeMinusOne] ? acc + curr : acc
+    ));
 }
 
 part1.forEach(input => check(input, captcha));
 part2.forEach(input => check(input, captcha));
+
+readFile('../input/01.txt')
+    .then((input) => {
+        check([input, 1092], captcha);
+    });
--- js/utils.js
+++ js/utils.js
@@ -1,34 +1,45 @@
-var fs = require('fs');
-var chalk = require('chalk');
+const fs = require('fs');
+const chalk = require('chalk');
 
-const readFile = (file) => new Promise((resolve, reject) => {
-        fs.readFile(file, 'utf8', (err,data) => {
-                err ? reject(err) : resolve(data);
-        })
+const readFile = file => new Promise((resolve) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        resolve(data) && data; // eslint-disable-line no-unused-expressions
+    });
 });
 
-const printP = (promisedRes) => promisedRes
-        .then(a => console.log(a))
-        .catch(err => console.log(err));
+const stringToArray = (numberString, seperator) => numberString
+    .split(seperator)
+    .map(n => parseInt(n, 10));
 
-function check([input, expected], meth) {
-        const res = meth(input);
+const printP = promisedRes => promisedRes
+    .then(a => console.log(a))
+    .catch(err => console.log(err));
 
-        if(res == expected) {
-                console.log(
-                        chalk.gray('===========================================\n') +
-                        chalk.green(' ✓ '), input, chalk.yellow(' ---> '), expected
-                );
-        } else {
-                console.log(
-                        chalk.gray('===========================================\n') +
-                        chalk.red(' x '), input, chalk.yellow(' ---> '), expected,
-                        '\n result was ', res
-                );
-        }
+function check([originalInput, expected], meth) {
+    const input = originalInput.length > 30
+        ? `${originalInput.slice(0, 30)}...`
+        : originalInput;
+
+    const res = meth(input);
+
+    if (res === expected) {
+        /* eslint-disable */
+        console.log(
+            chalk.gray('===========================================\n') +
+            chalk.green(' ✓ '), input, chalk.yellow(' ---> '), expected,
+        );
+    } else {
+        console.log(
+            chalk.gray('===========================================\n') +
+            chalk.red(' x '), input, chalk.yellow(' ---> '), expected,
+            '\n result was ', res,
+        );
+        /* eslint-enable */
+    }
 }
 module.exports = {
-        readFile,
-        printP,
-        check
+    stringToArray,
+    readFile,
+    printP,
+    check,
 };
