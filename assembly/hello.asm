25e992163141fa28f193618f1dfb21c8da89a449
Adam Hall <adamhalldesigns@gmail.com>
2018-05-07 19:04:09 +0100

    added some examples

--- assembly/hello.asm
+++ assembly/hello.asm
@@ -1,22 +1,22 @@
-BITS 64                     ; Hello.asm
+; ----------------------------------------------------------------------------------------
+; Writes "Hello, World" to the console using only system calls. Runs on 64-bit macOS only.
+; To assemble and run:
+;
+;     nasm -fmacho64 hello.asm && ld hello.o && ./a.out
+; ----------------------------------------------------------------------------------------
 
-section .data               ; beginning of our data section
+          global    start
 
-msg db "Hello, World",0xa   ; string with a carriage-return
-len equ  $ - msg            ; string length in bytes
+          section   .data
+message:  db        "Hello, World", 10      ; note the newline at the end
 
-section .text               ; start of the code indicator
-global start                ; make the main function externally visible
+          section   .text
+start:    mov       rax, 0x02000004         ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, message            ; address of string to output
+          mov       rdx, 13                 ; number of bytes
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 0x02000001         ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
 
-start:                      ; entry point for linker
-
-                            ; write our string to standard output
-mov rax, 0x2000004          ; system call number (sys_write)
-mov rdi, 1
-lea rsi, [msg wrt rip]
-mov rdx, len
-syscall; call the kernel
-
-xor rdi, rdi
-mov rax, 0x2000001          ; system call number (sys_exit)
-syscall                     ; make the system call
