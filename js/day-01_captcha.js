b0840ddaf44a9f718f5e349447d01e9f88e96a93
Adam Hall <adamhalldesigns@gmail.com>
2018-05-07 18:45:12 +0100

    about to get rid of catch

--- js/day-01_captcha.js
+++ js/day-01_captcha.js
@@ -2,8 +2,6 @@ const { READ, RUN } = require('./utils');
 const { stringToNumArr } = require('./stringTrasformers');
 const { d1: { part1, part2 } } = require('./tests');
 
-const inputFile = READ();
-
 const methodOne = input => stringToNumArr(input, '')
     .reduce((acc, cur, i, arr) => (
         cur === arr[ (i + 1) % arr.length ]
@@ -23,14 +21,14 @@ const methodTwo = input => stringToNumArr(input, '')
 /* --------------------------------------------- */
 
 part1.forEach(([input, expected]) => {
-    console.log('run');
-    RUN(Promise.resolve(input), expected, methodOne);
+    RUN(input, expected, methodOne);
 });
 
 part2.forEach(([input, expected]) => {
-    console.log('run2');
-    RUN(Promise.resolve(input), expected, methodTwo);
+    RUN(input, expected, methodTwo);
 });
 
-RUN(inputFile('01'), 1136, methodOne);
-RUN(inputFile('01'), 1092, methodTwo);
+READ('01').then((input) => {
+    RUN(input, 1136, methodOne);
+    RUN(input, 1092, methodTwo);
+}).catch(() => {});
--- js/utils.js
+++ js/utils.js
@@ -1,59 +1,52 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const RUN = async (inputP, expected, meth) => {
-    const line = '-------------------------------------------';
+const line = '-------------------------------------------';
+
+const RUN = (input, expected, meth) => {
+    const printableInput = JSON.stringify(input).slice(0, 25);
+    console.log(chalk.gray(line));
+
+    console.time(' ');
+    const res = meth(input);
+    console.timeEnd(' ');
+
+    if (res === expected) {
+        /* eslint-disable */
+        console.log(
+            chalk.green(' ✓ '), printableInput, chalk.yellow(' ---> '), expected,
+        );
+    } else {
+        console.log(
+            chalk.red(' x '), printableInput, chalk.yellow(' ---> '), expected,
+            '\n result was ', res,
+        );
+        /* eslint-enable */
+    }
+    console.log(chalk.gray(line));
+};
+
+const readFile = f => new Promise((res, rej) => {
+    fs.readFile(f, 'utf8', (err, data) => {
+        if (err) rej(new Error(err));
+        else { res(data); }
+    });
+});
+
+const READ = async (fileNumber, stringParseMethod = x => x) => {
     try {
-        const input = await inputP;
+        const data = await readFile(`../input/${fileNumber}.txt`);
+        const parsedData = stringParseMethod(data);
 
-        const printableInput = JSON.stringify(input).slice(0, 25);
-        console.log(chalk.gray(line));
-
-        console.time(' ');
-        const res = meth(input);
-        console.timeEnd(' ');
-
-        if (res === expected) {
-            /* eslint-disable */
-            console.log(
-                chalk.green(' ✓ '), printableInput, chalk.yellow(' ---> '), expected,
-            );
-        } else {
-            console.log(
-                chalk.red(' x '), printableInput, chalk.yellow(' ---> '), expected,
-                '\n result was ', res,
-            );
-            /* eslint-enable */
-        }
-        console.log(chalk.gray(line));
+        return parsedData;
     } catch (e) {
         console.log(
             chalk.red(line),
             '\n', e, '\n',
             chalk.red(line),
         );
+        throw e;
     }
-};
-
-const READ = () => {
-    const cache = {};
-    return (fileNumber, stringParseMethod = x => x) => new Promise((res, rej) => {
-        console.log(fileNumber);
-        console.log(cache);
-        if (fileNumber in cache) {
-            res(cache[ fileNumber ]);
-        }
-
-        console.log('did not hit cache');
-        fs.readFile(`../input/${fileNumber}.txt`, 'utf8', (err, data) => {
-            if (err) rej(new Error(err));
-            const parsedData = stringParseMethod(data);
-            cache[ fileNumber ] = parsedData;
-
-            console.log('returning');
-            res(parsedData);
-        });
-    });
 };
 
 module.exports = {
